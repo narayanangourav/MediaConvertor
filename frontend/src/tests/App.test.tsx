cb13424f3c0b62d529253dbb66047c70287f3fc9
narayanangourav <108493477+narayanangourav@users.noreply.github.com>
2025-11-28 22:12:20 +0530

    Project

--- frontend/src/tests/App.test.tsx
+++ frontend/src/tests/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { store } from "../store/store";
+import { store } from "../service/store";
 import App from "../App";
 import { describe, it, expect } from "vitest";
 
